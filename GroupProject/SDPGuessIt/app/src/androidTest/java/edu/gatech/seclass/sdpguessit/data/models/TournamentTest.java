3a426a164ccb070b5852faaea312822cee1dca09
rsilva32 <rsilva32@gatech.edu>
2018-03-01 17:37:59 -0500

    added a TC to test if the duplicate tournament name are allowed

--- GroupProject/SDPGuessIt/app/src/androidTest/java/edu/gatech/seclass/sdpguessit/data/models/TournamentTest.java
+++ GroupProject/SDPGuessIt/app/src/androidTest/java/edu/gatech/seclass/sdpguessit/data/models/TournamentTest.java
@@ -49,7 +49,7 @@ public class TournamentTest{
     }
 
     @Test
-    public void test1() {
+    public void testDBTournamentCreate() {
         Long puzzleID1 = puzzleM.createNewPuzzle(player,8,"Georgia Tech");
         Long puzzleID2 = puzzleM.createNewPuzzle(player,8,"Washington DC");
         Long puzzleID3 = puzzleM.createNewPuzzle(player,8,"United States of America");
@@ -65,9 +65,86 @@ public class TournamentTest{
         puzzleList.add(puzzleM.getPuzzle(puzzleID5));
 
         playerM.addNewPlayer("Team11", "Team11", "Team11", "Team11@gmail.com");
-
         tournamentM.createNewTournament(player,"Tournamnet1_Player1",puzzleList);
-
         assertEquals(playerM.doesUsenameExist("Team11"),true);
     }
+
+    @Test
+    public void testIfDuplicateTournamentNamesIgnored() {
+        Long puzzleID1 = puzzleM.createNewPuzzle(player,8,"Georgia Tech");
+        Long puzzleID2 = puzzleM.createNewPuzzle(player,8,"Washington DC");
+        Long puzzleID3 = puzzleM.createNewPuzzle(player,8,"United States of America");
+        Long puzzleID4 = puzzleM.createNewPuzzle(player,8,"Florida");
+        Long puzzleID5 = puzzleM.createNewPuzzle(player,8,"Georgia");
+
+        Long puzzleID6 = puzzleM.createNewPuzzle(player,8,"Georgia Tech");
+        Long puzzleID7 = puzzleM.createNewPuzzle(player,8,"Washington DC");
+        Long puzzleID8 = puzzleM.createNewPuzzle(player,8,"United States of America");
+        Long puzzleID9 = puzzleM.createNewPuzzle(player,8,"Florida");
+        Long puzzleID10 = puzzleM.createNewPuzzle(player,8,"Georgia");
+
+
+        List<Puzzle> puzzleListTournament1 = new ArrayList<>();
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID1));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID2));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID3));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID4));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID5));
+
+        List<Puzzle> puzzleListTournament2 = new ArrayList<>();
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID6));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID7));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID8));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID9));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID10));
+
+        playerM.addNewPlayer("Team11", "Team11", "Team11", "Team11@gmail.com");
+
+        Long idTournament1 = tournamentM.createNewTournament(player,"Tournamnet1_Player1",puzzleListTournament1);
+        Long idTournament2 = tournamentM.createNewTournament(player,"Tournamnet1_Player1",puzzleListTournament2);
+
+        assertEquals(tournamentM.getTournament(idTournament1).name,tournamentM.getTournament(idTournament2).name);
+    }
+
+    @Test
+    public void test() {
+
+        //Would need to change this TC after fixing the issue that Tournament name should be unique,
+        //which was detected by using this test and created a github issue.
+
+        Long puzzleID1 = puzzleM.createNewPuzzle(player,8,"Georgia Tech");
+        Long puzzleID2 = puzzleM.createNewPuzzle(player,8,"Washington DC");
+        Long puzzleID3 = puzzleM.createNewPuzzle(player,8,"United States of America");
+        Long puzzleID4 = puzzleM.createNewPuzzle(player,8,"Florida");
+        Long puzzleID5 = puzzleM.createNewPuzzle(player,8,"Georgia");
+
+        Long puzzleID6 = puzzleM.createNewPuzzle(player,8,"Georgia Tech");
+        Long puzzleID7 = puzzleM.createNewPuzzle(player,8,"Washington DC");
+        Long puzzleID8 = puzzleM.createNewPuzzle(player,8,"United States of America");
+        Long puzzleID9 = puzzleM.createNewPuzzle(player,8,"Florida");
+        Long puzzleID10 = puzzleM.createNewPuzzle(player,8,"Georgia");
+
+
+        List<Puzzle> puzzleListTournament1 = new ArrayList<>();
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID1));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID2));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID3));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID4));
+        puzzleListTournament1.add(puzzleM.getPuzzle(puzzleID5));
+
+        List<Puzzle> puzzleListTournament2 = new ArrayList<>();
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID6));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID7));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID8));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID9));
+        puzzleListTournament2.add(puzzleM.getPuzzle(puzzleID10));
+
+        playerM.addNewPlayer("Team11", "Team11", "Team11", "Team11@gmail.com");
+
+ //       Long idTournament1 = tournamentM.createNewTournament(player,"Tournamnet1_Player1",puzzleListTournament1);
+ //       Long idTournament2 = tournamentM.createNewTournament(player,"Tournamnet1_Player1",puzzleListTournament2);
+
+ //       assertEquals(tournamentM.getTournament(idTournament1).name,tournamentM.getTournament(idTournament2).name);
+    }
+
 }
